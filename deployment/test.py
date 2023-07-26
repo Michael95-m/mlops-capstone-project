cbaf5ad0d41bcd41954ae45c65caa8b1784a6d6e
Michael95-m <minkhantmgmg.mk19@gmail.com>
2023-07-26 11:10:44 +0630

    testing file for service

---
+++ deployment/test.py
@@ -0,0 +1,11 @@
+import requests
+import json 
+
+with open("sample_data.json") as f_in:
+    payloads = json.load(f_in)
+
+for payload in payloads:
+    url = "http://127.0.0.1:5010/predict"
+    response = requests.post(url, json=payload)
+    print(f"For payload: {payload}")
+    print(f"Json data is {response.json()}")
