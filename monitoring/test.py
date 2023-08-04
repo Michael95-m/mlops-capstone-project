8d9f68716ff21be095f1b5190283deffbf2f22ca
Michael95-m <minkhantmgmg.mk19@gmail.com>
2023-08-04 11:07:56 +0630

    to update

--- monitoring/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-import os
-
-MONITOR_SERVE_ADDRESS = os.getenv("MONITOR_SERVE_ADDRESS", "http://127.0.0.1:5020")
-
-full_url = f"{MONITOR_SERVE_ADDRESS}/monitor_target_drift?window_size=600"
-file = requests.get(full_url)
