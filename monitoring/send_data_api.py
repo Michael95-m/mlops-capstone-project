fffa103f0b6faee428a00111bf8d562944a58f78
Michael95-m <minkhantmgmg.mk19@gmail.com>
2023-08-04 11:07:31 +0630

    sending data to api

--- monitoring/send_data_api.py
+++ monitoring/send_data_api.py
@@ -1,6 +1,5 @@
 import requests
 import pandas as pd
-import os
 from time import sleep
 
 from utils.utils import get_test_path, get_monitor_serve_url
@@ -11,12 +10,16 @@ if __name__ == "__main__":
     test_df = pd.read_parquet(test_path)
     test_df.columns = test_df.columns.str.lower()
     test_df.drop("diabetes", axis=1, inplace=True)
+    test_df = test_df.sample(frac=1).reset_index(drop=True)
 
     test_data = test_df.to_dict(orient="records")
 
     monitor_serve_url = get_monitor_serve_url()
 
-    for payload in test_data:
-        resp = requests.post(monitor_serve_url, json=payload).json()
-        sleep(1)
+    while True:
+        print("start sending the data...")
+        for payload in test_data:
+            print("sending the data...")
+            resp = requests.post(monitor_serve_url, json=payload).json()
+            sleep(1)
     
